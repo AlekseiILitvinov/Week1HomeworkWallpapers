ffecb83fe6b75ac0ab690a92aaa66ca2719d1af3
AlekseiILitvinov <litvinovai988@gmail.com>
2019-09-16 22:55:04 +0300

    Changed the pattern shift logic. Now it takes into consideration the pattern size.

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
 		double roomLength = 5.5;
 		double roomHeight = 2.7;
 		double shiftPattern = 0.15;
+		double patternSize = 0.3;
 		double wallpaperWidth = 1.06;
 
 		//Uncomment these lines for additional interactivity
@@ -24,6 +25,8 @@ public class Main {
 //		roomLength = Double.parseDouble(scanner.next());
 //		System.out.print("Please enter the heights of the ceiling in meters: ");
 //		roomHeight = Double.parseDouble(scanner.next());
+//		System.out.print("Please enter the pattern (rapport)size in cm (enter 0 if  pattern matching is not needed): ");
+//		patternSize = Double.parseDouble(scanner.next()) / 100; //adjusting to meters
 //		System.out.print("If the wallpaper has pattern (rapport) shift, please enter the enter the value of the shift" +
 //				" in cm, or enter 0 otherwise: ");
 //		shiftPattern = Double.parseDouble(scanner.next()) / 100; //adjusting to meters
@@ -31,10 +34,14 @@ public class Main {
 		double roomPerimeter = (roomWidth + roomLength) * 2;
 		int stripsNeeded = (int) Math.ceil(roomPerimeter / wallpaperWidth);
 
-		double stripLength = roomHeight + 0.1 + shiftPattern;
+		double stripLength = roomHeight + 0.1;
+		if (patternSize > 0.009) { //only applied if the pattern is larger than 1cm
+			stripLength = Math.ceil(stripLength / patternSize) * patternSize + shiftPattern + 0.1;
+			//Not taking into account possible case of backwards shift. Let's say the store wants to sell more.
+		}
 		int stripsInRoll = (int) (10 / stripLength);
 
-		int rollsNeeded = (stripsNeeded +  stripsInRoll - 1) / stripsInRoll; //A mathematical hack instead of ceil()
+		int rollsNeeded = (stripsNeeded + stripsInRoll - 1) / stripsInRoll; //A mathematical hack instead of ceil()
 		System.out.println("You would need " + rollsNeeded + " rolls of selected wallpapers");
 
 	}
